650ebb242bb42082a67b998c8663e1a6f8ee721d
varshasaini1868 <sainivarsha1868@gmail.com>
2026-07-09 19:02:59 +0530

    Time: 3 ms (92.69%), Space: 44.6 MB (94.83%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < nums1.length; i++) {
             int index = -1;
 
-            // nums2 me element find karo
+           
             for (int j = 0; j < nums2.length; j++) {
                 if (nums2[j] == nums1[i]) {
                     index = j;
@@ -13,7 +13,7 @@ class Solution {
                 }
             }
 
-            // right side me next greater element find karo
+            
             ans[i] = -1;
             for (int j = index + 1; j < nums2.length; j++) {
                 if (nums2[j] > nums1[i]) {
