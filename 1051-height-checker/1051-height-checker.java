69fd962f8cd41914ed38a9503106ab96a2cf8a01
varshasaini1868 <sainivarsha1868@gmail.com>
2026-07-19 18:48:57 +0530

    Time: 4 ms (80.67%), Space: 43.4 MB (45.43%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count = 0;
+
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+   
+}
